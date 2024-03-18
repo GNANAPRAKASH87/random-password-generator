e801d20a6dccdabc524d526c7219b3f1c9703a4a
GNANAPRAKASH87 <159160778+GNANAPRAKASH87@users.noreply.github.com>
2024-03-18 19:02:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+const lengthSlider=document.querySelector(".pass-length input"),
+options=document.querySelectorAll(".option input"),
+copyIcon=document.querySelector(".input-box span"),
+passwordInput=document.querySelector(".input-box input"),
+generateBtn=document.querySelector(".generate-btn");
+const characters={
+    lowercase:"abcefghijklmnopqrstuvwxyz",
+    uppercase:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers:"1234567890",
+    symbols:"!@#$%^&*()[]{};,.<>:"
+}
+const generatePassword=()=>{
+ let staticPassword="",randomPassword="",excludeDuplicate=false,
+ passLength=lengthSlider.value;
+ options.forEach(option=>{
+      if(option.checked){
+        if(option.id !== "exc-duplicate" && option.id !== "spaces"){
+        staticPassword+=characters[option.id];
+        }else if(option.id==="spaces"){
+            staticPassword+=` ${staticPassword} `;
+        }else{
+            excludeDuplicate=true;
+        }
+      }
+ });
+
+ for(let i=0;i<passLength;i++){
+    let randomChar =staticPassword[Math.floor(Math.random()*staticPassword.length)];
+     if(excludeDuplicate){
+        !randomPassword.contains(randomChar) || randomChar==" " ? randomPassword+=randomChar:i--;
+     }else{
+        randomPassword+=randomChar;
+     }
+
+ }
+
+ passwordInput.value=randomPassword;
+}
+const updatSlider=()=>{
+    document.querySelector(".pass-length span").innerText=lengthSlider.value;
+}
+updatSlider();
+
+const copyPassword=()=>{
+    navigator.clipboard.writeText(passwordInput.value);
+    copyIcon.innerText="check";
+    setTimeout(()=>{
+        copyIcon.innerText="copy_all";
+    },1500);
+}
+
+
+copyIcon.addEventListener("click",copyPassword);
+lengthSlider.addEventListener("input",updatSlider);
+generateBtn.addEventListener("click",generatePassword);
